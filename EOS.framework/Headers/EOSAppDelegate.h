3bbf2ffdab11398b3eb3d6305b7d7bbfa084ab0c
oxro <sam@Sams-MacBook-Air.local>
2014-08-18 13:15:21 +0800

    update 23

--- EOS.framework/Headers/EOSAppDelegate.h
+++ EOS.framework/Headers/EOSAppDelegate.h
@@ -31,6 +31,16 @@
     UILabel *busyLabel;
     
     NSInteger busyCount;
+    
+    NSTimeInterval lastChangeViewStack;
+}
+
+#define PROTECT_VIEW_STACK(blk)    \
+if ([NSDate timeIntervalSinceReferenceDate] - lastChangeViewStack < 0.5) {    \
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)((0.5 - ([NSDate timeIntervalSinceReferenceDate] - lastChangeViewStack)) * NSEC_PER_SEC)), dispatch_get_main_queue(), blk); \
+    return YES;  \
+} else {    \
+    lastChangeViewStack = [NSDate timeIntervalSinceReferenceDate];  \
 }
 
 @property (strong, nonatomic) UIWindow *window;
