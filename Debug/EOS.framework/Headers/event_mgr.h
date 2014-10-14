e180500f4f3fd9118f832e5474cf8a25d7f560e6
oxro <sam@Sams-MacBook-Air.local>
2014-10-14 15:21:55 +0800

    update

--- Debug/EOS.framework/Headers/event_mgr.h
+++ Debug/EOS.framework/Headers/event_mgr.h
@@ -10,6 +10,10 @@
 #define __EOSFramework__event_mgr__
 
 #include <stdio.h>
+#include <event2/event.h>
+#include <event.h>
+#include <event2/dns.h>
+#include <event2/thread.h>
 
 struct event_base *event_mgr_base();
 struct evdns_base *event_mgr_dnsbase();
